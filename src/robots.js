fc1510d0f5ac9635771ba67b65f9b0233f89f0a7
Brian Kenney <bk@MacBook-Pro.local>
2021-09-14 13:32:18 -0400

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,20 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Bee Kay',
+		username: 'BK',
+		email: 'bk@gmail.com'
+	},
+	{
+		id: 2,
+		name: 'Bellz',
+		username: 'BW',
+		email: 'bw@gmail.com'
+	},
+	{
+		id: 3,
+		name: 'Bean',
+		username: 'Beandag69',
+		email: 'beandag69@gmail.com'
+	}
+]
